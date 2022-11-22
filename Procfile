02caa1d39d00f966de17ff88143ce5c2769341f0
sourav-000075 <96191412+sourav-000075@users.noreply.github.com>
2022-11-22 18:44:16 +0530

    added p.py and Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: pip install streamlit && streamlit run p.py
---
+++ p.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('Adding 2 numbers')
+def user_input():
+    n1 = st.number_input("n1")
+    n2 = st.number_input("n2")
+    data={
+        'n1':n1,
+        'n2':n2
+    }
+    features = pd.DataFrame(data, index=[0])
+    return features,n1,n2
+
+df,n1,n2 = user_input()
+
+st.subheader('User Input parameters')
+st.write(df.to_dict())
+st.subheader('Sum:')
+st.write(n1+n2)
