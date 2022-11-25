a35757db71e5a871d4c686a32114b8e535e1e66a
sourav-000075 <96191412+sourav-000075@users.noreply.github.com>
2022-11-25 18:44:37 +0530

    Update p.py

--- p.py
+++ p.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 st.title('Adding 2 numbers')
 def user_input():
-    n1 = st.number_input("n1")
-    n2 = st.number_input("n2")
+    n1 = st.number_input("n1",step=1)
+    n2 = st.number_input("n2",step=1)
     data={
         'n1':n1,
         'n2':n2
@@ -14,9 +14,9 @@ def user_input():
     return n1,n2
 
 n1,n2 = user_input()
-
+print('here')
 #st.subheader('User Input parameters')
 #st.write(df.to_dict())
 st.subheader('Sum:')
 print(n1+n2)
-st.write(str(n1+n2))
+st.write(n1+n2)
