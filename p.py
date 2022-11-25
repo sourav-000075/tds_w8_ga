1e9df450776c8b0e1b9235a7f3b453558a4fd23c
sourav-000075 <96191412+sourav-000075@users.noreply.github.com>
2022-11-25 18:47:07 +0530

    Update p.py

--- p.py
+++ p.py
@@ -4,16 +4,16 @@ import pandas as pd
 
 st.title('Adding 2 numbers')
 def user_input():
-    n1 = st.number_input("n1",step=1)
-    n2 = st.number_input("n2",step=1)
+    n1 = st.number_input("n1")
+    n2 = st.number_input("n2")
     data={
         'n1':n1,
         'n2':n2
     }
-    #features = pd.DataFrame(data, index=[0])
-    return n1,n2
+    features = pd.DataFrame(data, index=[0])
+    return features,n1,n2
 
-n1,n2 = user_input()
+df,n1,n2 = user_input()
 print('here')
 #st.subheader('User Input parameters')
 #st.write(df.to_dict())
