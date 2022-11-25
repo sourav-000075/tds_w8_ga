4ddf4cfde98e26b6dfc3530244a3cdfa3164adf5
sourav-000075 <96191412+sourav-000075@users.noreply.github.com>
2022-11-25 18:21:31 +0530

    Update p.py

--- p.py
+++ p.py
@@ -11,11 +11,11 @@ def user_input():
         'n2':n2
     }
     features = pd.DataFrame(data, index=[0])
-    return n1,n2
+    return features,n1,n2
 
 df,n1,n2 = user_input()
 
 st.subheader('User Input parameters')
 st.write(df.to_dict())
 st.subheader('Sum:')
-st.write(n1+n2)
+st.write(int(n1+n2))
